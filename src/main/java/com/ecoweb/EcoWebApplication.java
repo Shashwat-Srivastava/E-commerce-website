8a754779b80bf8adf5865fc70a977893ead03450
Sheersh <sheershcooper@gmail.com>
2022-02-03 01:11:18 +0530

    Project Structure Commit

---
+++ src/main/java/com/ecoweb/EcoWebApplication.java
@@ -0,0 +1,14 @@
+package com.ecoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoWebApplication.class, args);
+		System.out.println("Test...............................................");
+	}
+
+}
---
+++ src/test/java/com/ecoweb/EcoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
